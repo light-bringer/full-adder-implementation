039e0b061ced8caae1f3828859d1675492e23149
Azhor Ahai <debapriya.das.blr@gmail.com>
2017-12-09 22:43:51 +0530

    autotest to check the logic is correct or not

--- testAdder.py
+++ testAdder.py
@@ -4,11 +4,22 @@
 from Adder import *
 import random
 
-def randomgen():
-    for x in range(10):
-        num1 = random.randint(1,255)
-        num2 = random.randint(1,255)
-        print ("Sum : ", num1 + num2)
+def randomtest():
+    testcount = 0
+    failed = 0
+    for x in range(100):
+        num1 = random.randint(0,256)
+        num2 = random.randint(0,256)
+        sum = num1 + num2
+        binarysum = EightBitAdder(to8bits(num1), to8bits(num2))
+
+        if sum ==  tobase10(binarysum):
+            testcount = testcount + 1
+        else:
+            failed = failed + 1
+
+    print("passed : ", testcount)
+    print("failed : ", failed)
 
 
-randomgen()
+randomtest()
